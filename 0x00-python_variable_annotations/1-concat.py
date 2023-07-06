6fe41376363de7ed3b23cef9a8edafa1a48dd31a
Roylouis-web <powellleroy488@gmail.com>
2023-07-06 19:58:05 +0100

    All done

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -11,4 +11,4 @@ def concat(str1: str, str2: str) -> str:
         and a string str2 as arguments and returns a concatenated string
     """
 
-    return '{}{}'.format(str1, str2)
+    return str1 + str2
